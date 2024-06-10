033c54d98937d2f262786ad758ea1cc986a7040f
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-09 21:37:23 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int retNum = (int) nums.size();
+        unordered_map<int, int> alwaysGiveGood;
+        for(int x = 0; x < retNum; x++)
+        {
+            int compliment = target - nums[x]; // I'm playing with words here.  complement vs. compliment
+            if(alwaysGiveGood.count(compliment))
+            {
+                return {x, alwaysGiveGood[compliment]};
+            }
+            alwaysGiveGood[nums[x]] = x;
+        }
+        return {-1, -1};
+    }
+};
